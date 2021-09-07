3ccc475b03b18893fdd7133f8cda6be5e3e7c0f8
Roderic Russell <rrr5458@gmail.com>
2021-09-07 15:18:09 -0400

    add routes and react client

--- routes/users.js
+++ routes/users.js
@@ -10,6 +10,7 @@ router.post('/register', function(req, res, next) {
     res.status(400).json({
       error: 'please include username and password'
     })
+    return
   }
 
   // check if username is taken
@@ -43,5 +44,45 @@ router.post('/register', function(req, res, next) {
         })
     })
 });
+
+router.post('/login', async (req, res) => {
+  // check if username and password
+  if (!req.body.username || !req.body.password) {
+    res.status(400).json({
+      error: 'please include username and password'
+    })
+    return
+  }
+
+  // find user by username
+  const user = await db.User.findOne({
+    where: {
+      username: req.body.username
+    }
+  })
+
+  if (!user) {
+    res.status(400).json({
+      error: 'could not find user with that username'
+    })
+    return
+  }
+
+  // check password
+  const success = await bcrypt.compare(req.body.password, user.password)
+  if (!success) {
+    res.status(401).json({
+      error: 'incorrect password'
+    })
+    return
+  }
+  // login
+  // req.session.user = user
+
+  // respond with success/error
+  res.json({
+    success: 'Successfully logged in'
+  })
+})
 
 module.exports = router;
